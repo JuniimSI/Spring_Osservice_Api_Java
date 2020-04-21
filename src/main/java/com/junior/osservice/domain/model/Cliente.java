60f605b862860d08824c6e7ada8e6d0303db9d70
Francisco Fernandes Júnior <jrfernandesti@gmail.com>
2020-04-20 23:59:40 -0300

    Initial commit

---
+++ src/main/java/com/junior/osservice/domain/model/Cliente.java
@@ -0,0 +1,33 @@
+package com.junior.osservice.domain.model;
+
+public class Cliente {
+	private String nome;
+	private Long id;
+	private String email;
+	private String telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
